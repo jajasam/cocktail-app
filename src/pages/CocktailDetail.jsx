0da4b954c15216a1729107f0da6f5de01ef3ea25
Jana <jana.samson@hotmail.fr>
2022-12-18 10:19:37 -0500

    get cocktail data based on route id

--- src/pages/CocktailDetail.jsx
+++ src/pages/CocktailDetail.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import '../styles/CocktailDetail.css'
 
 function CocktailDetail() {
+  const cocktailId = useParams().id
+  const [cocktailData, setCocktailData] = useState()
+
+  useEffect(() => {
+    if(!cocktailId) return;
+
+    //get cocktail's data by id
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
+    .then(res => res.json())
+    .then(data => setCocktailData(data))
+  }, [cocktailId])
   
   return (
     <main className="cocktail-detail">
