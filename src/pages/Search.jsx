0dab75242cff40bc2cbeff6729a0dd70a5df55be
Jana <jana.samson@hotmail.fr>
2022-12-17 20:32:51 -0500

    get filters by category for the search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,15 +3,58 @@ import { useEffect, useState } from 'react'
 import '../styles/Search.css'
 
 function Search() {
-  const [results, setResults] = useState(null)
+  const [results, setResults] = useState()
+  const [filters, setFilters] = useState([
+    {
+      category: 'Categories',
+      param: 'c',
+      subFilters: []
+    },
+    {
+      category: 'Alcohols',
+      param: 'a',
+      subFilters: []
+    },
+    {
+      category: 'Glasses',
+      param: 'g',
+      subFilters: []
+    },
+    {
+      category: 'Ingredients',
+      param: 'i',
+      subFilters: []
+    }
+  ])
 
   useEffect(() => {
+      //get random cocktails when there are no filters
       fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then(res => res.json())
       .then(data => setResults(data.drinks))
       .catch(err => console.error(err))
   }, []);
-  console.log(results)
+
+  useEffect(() => {
+    //set filters 
+    filters.map(elem => fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?${elem.param}=list`)
+    .then(res => res.json())
+    .then(data => setFilters(prev => [
+      ...prev,
+      {
+        category: elem.category,
+        param: elem.param,
+        subFilters: data.drinks
+      }
+    ]
+  )))}, [])
+
+  const filtersElem = filters?.map((filter,i)  => <div key={i}>
+    <p>{filter.category}</p>
+    {
+      filter.subFilters.map((el, i) => <p key={i}>{Object.values(el)}</p>)
+    }
+  </div>)
 
   return (
     <>
@@ -31,18 +74,10 @@ function Search() {
         </section>
         <section className="results">
           <div className="filters">
-            <div className="filter">
-              Alcohol
-            </div>
-            <div className="filter">
-              Ingredients
-            </div>
-            <div className="filter">
-              Glasses
-            </div>
-            <div className="filter">
-              Category
-            </div>
+              {
+                filtersElem &&
+                filtersElem
+              }
           </div>
           <div className="cocktails">
             <div className="cocktail"></div>
