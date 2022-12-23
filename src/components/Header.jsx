f6b07907b39ac7557d5367f0a704e136db5cf78e
Jana <jana.samson@hotmail.fr>
2022-12-23 08:38:26 -0500

    use debounce to reduce frequency of scrolling events on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,18 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import debounce from 'lodash.debounce'
 
 import '../styles/Header.css'
 
 function Header() {
     const [topOffset, setTopOffset] = useState(0)
-    window.addEventListener('scroll', (e) => {
+    window.addEventListener('scroll', debounce(() => {
         setTopOffset(window.pageYOffset)
-    })
-
+    }, 25))
+    
   return (
     <header className={topOffset > 0 ? 'sticky' : ''}>
         <div className="content">
-            <nav>
-                <ul>
-                    <li>
-                        Search Cocktails by Name
-                    </li>
-                    <li>
-                        Search Cocktails by Ingredients
-                    </li>
-                </ul>
-            </nav>
             <Link to="/">
                 <img 
                 src={require('../assets/logo.png')} 
@@ -29,11 +20,6 @@ function Header() {
                 className={`logo ${topOffset > 0 ? 'small' : ''}`}
                 />
             </Link>
-            <div className="hamburger-icon">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
         </div>
     </header>
   )
