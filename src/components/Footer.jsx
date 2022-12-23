a485e18410d9f20ca32039cf78b953cc557a92a6
Jana <jana.samson@hotmail.fr>
2022-12-22 19:53:57 -0500

    refactor footer styling and add copyright statement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,7 @@ import '../styles/Footer.css'
 function Footer() {
   return (
     <footer>
-      <img 
-          src={require('../assets/footer-img.jpg')} 
-          alt="Cocktails view from top" 
-          width="70%"
-          height="400px"
-        />
-        <img 
-          src={require('../assets/logo.png')} 
-          alt="Cheers! Logo" 
-          width="100px"
-          height="100px"
-        />
-        <p>Discover a world of flavors</p> 
+        <small>Copyright © 2022 - Jana Samson</small>
     </footer>
   )
 }
